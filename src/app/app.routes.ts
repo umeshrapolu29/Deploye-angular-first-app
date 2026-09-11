14df2557456aacecee974b796454a9cec737aa48
umeshrapolu29 <umeshrapolu29@gmai.com>
2026-09-11 21:20:30 +0530

    create login and registration form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'login',
+		loadComponent: () => import('./layouts/login-page/login-page').then((component) => component.LoginPage)
+	},
+	{
+		path: 'employee-registration',
+		loadComponent: () =>
+			import('./layouts/employee-registration/employee-registration').then(
+				(component) => component.EmployeeRegistration
+			)
+	},
+	{ path: 'register', pathMatch: 'full', redirectTo: 'employee-registration' },
+	{ path: '', pathMatch: 'full', redirectTo: 'login' },
+	{ path: '**', redirectTo: 'login' }
+];
